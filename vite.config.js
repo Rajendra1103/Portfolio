392a337af13241f32a06128caf4221db5520c052
Rajendra1103 <rajendrahs36@gmail.com>
2025-07-12 12:52:23 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Portfolio/",
+  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
